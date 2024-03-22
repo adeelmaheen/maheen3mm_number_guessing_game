7f3dc61b85024ea0f3ef97705e7e552a5799ac1b
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-03-22 16:00:08 -0700

    commit

---
+++ app.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+// 1) computer will generate a random number
+// 2)  user input for guessing number
+// 3)  comparing random number to random number 
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const ask = await inquirer.prompt([
+    {
+        name: "userguessedNumber",
+        type: "number",
+        message: chalk.italic.bold.green("Please guess a number between 1 to 6")
+    }
+]);
+console.log(ask);
+if (ask.userguessedNumber === randomNumber) {
+    console.log(chalk.bold.italic.green("Congratulations! you guessed a right number."));
+}
+else {
+    console.log(chalk.bold.italic.green("Oops! Wrong number,Try Again."));
+}
